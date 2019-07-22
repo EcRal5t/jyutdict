bdb7de99140fbbc8c7b9ee5eb50374a995d3375b
Zenam <liangzinan21@hotmail.com>
2019-07-22 17:03:16 +0800

    Fixed bugs When displaying Multi map --Zenam

--- Lookup.class.php
+++ Lookup.class.php
@@ -9,19 +9,19 @@ class Sim2TradLookup extends Lookup {
     private function _construct() {
         self::$instance = null;
     }
-    
+
     private static $instance;
     private function _clone() {}
-    
+
     public static function getInstance() {
         if (!self::$instance instanceof Sim2TradLookup) {
             self::$instance = new Sim2TradLookup();
         }
         return self::$instance;
     }
-    
+
     public function query($character, $dbh) {   ///到時候會大改一遍的…
-        $charaArray = array($character);
+        $charaArray = array($character);		//改的时候请好好规划
         $sim2Trad_getCharaId_sql = "
             SELECT chara_id
             FROM `Character_simtrad_list`
@@ -29,7 +29,7 @@ class Sim2TradLookup extends Lookup {
         $sim2Trad_getCharaId_stmt = $dbh->prepare($sim2Trad_getCharaId_sql);
         $sim2Trad_getCharaId_stmt->execute(array(':chara'=>$character));
         $sim2Trad_getCharaId_result = $sim2Trad_getCharaId_stmt->fetchAll(PDO::FETCH_ASSOC);
-        
+
         #执行SQL语句返回结果集数列
         if ($sim2Trad_getCharaId_result!=[]) {#如果结果集存在
             $sim2Trad_SimMap_sql = "
@@ -39,7 +39,7 @@ class Sim2TradLookup extends Lookup {
             $sim2Trad_SimMap_stmt = $dbh->prepare($sim2Trad_SimMap_sql);
             $sim2Trad_SimMap_stmt->execute();
             $sim2Trad_SimMap_result = $sim2Trad_SimMap_stmt->fetchAll(PDO::FETCH_ASSOC);
-            
+
             foreach ($sim2Trad_SimMap_result as $items) {
                 $sim2Trad_getTradChara_sql = "
                     SELECT `chara`
@@ -47,14 +47,14 @@ class Sim2TradLookup extends Lookup {
                     WHERE `chara_id` =" . $items['chara_id_trad'];
                 $sim2Trad_getTradChara_stmt = $dbh->prepare($sim2Trad_getTradChara_sql);
                 $sim2Trad_getTradChara_stmt->execute();
-                
+
                 $result = $sim2Trad_getTradChara_stmt->fetchAll(PDO::FETCH_ASSOC);
                 if ($character != $result[0]['chara']) array_push($charaArray, $result[0]['chara']);
             }#end foreach
         }#end if(!=[])
         return $charaArray;
     }#end function query
-    
+
     public function show($charaArray) {
         if (is_array($charaArray)) {
             $count = count($charaArray);
@@ -79,11 +79,11 @@ class Sim2TradLookup extends Lookup {
 
 class FanWanDict extends Lookup {
     private function _clone() {}
-    
+
     final private function __construct() {
         self::$instance = null;
     }
-    
+
     protected static $instance;
     static public function getInstance() {
         if (self::$instance instanceof self) {
@@ -93,7 +93,7 @@ class FanWanDict extends Lookup {
             return self::$instance;
         }
     }
-    
+
     function query($character, $dbh) {
         $inFanwan_sql = "
             SELECT `id`, `chara`, `initial`, `nuclei`, `coda`, `tone`, `siuwan`, `meaning`, `initial_ch`, `final_ch`, `yunbu`, `tone_ch`
@@ -103,14 +103,14 @@ class FanWanDict extends Lookup {
         $inFanwan_stmt->execute(array(':chara'=>$character));
         return $inFanwan_stmt->fetchAll(PDO::FETCH_ASSOC);
     }//end function query
-    
+
     function show($charArray) {
         if(empty($charArray)) {
             echo "<span style='font-size: 20px;'>分韻冇見有</span>";
         } else {
             $jyutping = new Jyutping();
             ?>
-            
+
             <table class="general-form annex-form">
                 <?PHP
                 foreach ($charArray as $resultItem) { #将数组一个个输出
@@ -133,27 +133,27 @@ class FanWanDict extends Lookup {
                         <td>分韻</td>
                         <td colspan="4" style="border-top: none;"><?PHP echo $resultItem['meaning'] ?></td>
                     </tr>
-                    
+
                     <?PHP
                 }//end foreach
                 ?>
             </table>
-            
+
             <?PHP
         }//end if
     }//end function show
 }
 
 class JingWaaDict extends Lookup {
-    
+
     private function _clone() {}
-    
+
     final private function __construct() {
         self::$instance = null;
     }
-    
+
     static protected $instance;
-    
+
     static public function getInstance() {
         if (self::$instance instanceof self) {
             return self::$instance;
@@ -162,7 +162,7 @@ class JingWaaDict extends Lookup {
             return self::$instance;
         }
     }
-    
+
     function query($character, $dbh) {
         $inJingwaa_sql = "
             SELECT `id`, `page`, `initial`, `nuclei`, `coda`, `tone`, `pron`, `radical`, `radical_stroke`, `extra_stroke`, `page`, `state`, `order`
@@ -172,7 +172,7 @@ class JingWaaDict extends Lookup {
         $inJingwaa_stmt->execute(array(':chara'=>$character));
         return $inJingwaa_stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    
+
     function show($charArray) {
         if (empty($charArray)) {
             echo "<span style='font-size: 20px;'>英華冇見有</span>";
@@ -203,7 +203,7 @@ class JingWaaDict extends Lookup {
                     $lastOrder = $resultItem['order'];
                 }
                 ?>
-                
+
             </table>
             <?PHP
         }//end if
@@ -215,19 +215,23 @@ interface displayInMap {
 }
 
 class LocalDictionary extends Lookup implements displayInMap {
+    private static $count = 0;
+
     private function _construct() {}
-    
+
     private static $instance;
-    
+
     private function _clone() {}
-    
+
     public static function getInstance() {
         if (!self::$instance instanceof LocalDictionary) {
             self::$instance = new LocalDictionary();
+        }else{
+            self::$count++;
         }
         return self::$instance;
     }
-    
+
     private function getCityList($dbh) {
         $inCityList_sql = "
             SELECT `longitude`, `latitude`, `first`, `second`, `third`, `sheetname`
@@ -236,7 +240,7 @@ class LocalDictionary extends Lookup implements displayInMap {
         $inCityList_stmt->execute();
         return $inCityList_stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    
+
     public function query($character, $dbh) {
         $cityListArray = $this->getCityList($dbh);
         $resultArray = [];
@@ -249,7 +253,7 @@ class LocalDictionary extends Lookup implements displayInMap {
         }
         return $resultArray;
     }
-    
+
     private function charaQueryInSheet($character, $sheetName, $dbh) {
         $inSheet_sql = "
             SELECT `chara`, `initial`, `nuclei`, `coda`, `tone`, `ipa`, `note`
@@ -259,7 +263,7 @@ class LocalDictionary extends Lookup implements displayInMap {
         $inSheet_stmt->execute(array(':chara'=>$character));
         return $inSheet_stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    
+
     public function show($charaArray, $showMap=TRUE) {
         if(!empty($charaArray)){
             $character = $charaArray[0][0]['chara']; #从第一个结果里面取得字
@@ -325,12 +329,16 @@ class LocalDictionary extends Lookup implements displayInMap {
     function display($charaArray) {
         if(!empty($charaArray)) {
             //print_r($charaArray);
+
+            echo "<script>var count = ".self::$count.";</script>";
+            echo "<div class=\"generalBgDeeper\" id=\"mapContainer".self::$count."\"></div>";
             ?>
-            <div class="generalBgDeeper" id="mapContainer"></div>
+            <script src="https://webapi.amap.com/maps?v=1.4.13&key=160f3ffdbe10ec13c75edac2fae17e3c"></script>
             <script type="text/javascript">
-                var amap;
-                window.init = function() {
-                    amap = new AMap.Map('mapContainer', {
+                var container = "mapContainer" + count;
+                window["amap" + count];
+
+                    window["amap" + count] = new AMap.Map(container, {
                         zoom : 6,
                         resizeEnable: true,
                         center: [111.08,23.43],                         //中心点坐标
@@ -342,7 +350,7 @@ class LocalDictionary extends Lookup implements displayInMap {
                         $longitude = $charaArray[$num]['longitude'];
                         $latitude = $charaArray[$num]['latitude'];
                         #此处CONTENT是MAKER标记位置的内容 去掉默认CONTENT是一个小大头针
-                        
+
                         echo "var marker$num = new AMap.Marker({";
                             $pron = "";
                             for($charaNum = 0; $charaNum < (count($charaArray[$num]) - 6); $charaNum++) {
@@ -355,14 +363,12 @@ class LocalDictionary extends Lookup implements displayInMap {
                             echo "position: [$longitude,$latitude],";
                             echo "content: \"<div class='locale-label'>$pron<div class='label-triangle'></div></div>\"";
                         echo "});";
-                        
-                        echo "marker$num.setMap(amap);";
+
+                        echo "marker$num.setMap(window[\"amap\"+count]);";
                     }#end for num
                     ?>
-                }
+
             </script>
-            <script src="https://webapi.amap.com/maps?v=1.4.13&key=160f3ffdbe10ec13c75edac2fae17e3c&callback=init"></script>
-            
             <?PHP
         }
     }
