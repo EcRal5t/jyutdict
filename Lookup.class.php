eec64427c520dbafd93d34788107c7258a72db78
Zenam <liangzinan21@hotmail.com>
2019-08-20 19:14:04 +0800

    hmmm地圖CSS JS改返在線嘅好

--- Lookup.class.php
+++ Lookup.class.php
@@ -253,9 +253,13 @@ class LocalDictionary extends Lookup implements displayInMap {
             </style>
             <script>var mapList = new Array();/*用于存储地图列表*/</script>
             <!-- leaflet 的前置CSS和JS -->
-             <link rel="stylesheet" href="css/leaflet.css"/>
+            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
+  integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+  crossorigin=""/>
                <!-- Make sure you put this AFTER Leaflet's CSS -->
-            <script src="js/leaflet.js"></script>
+               <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
+  integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
+  crossorigin=""></script>
               <?PHP
         }else{
             self::$count++;
