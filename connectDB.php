e72ab840cb1b3553cd0a9695003c075d0e1207ee
root <root@vultr.guest>
2019-10-14 21:10:18 +0800

    sync server version

--- connectDB.php
+++ connectDB.php
@@ -1,5 +1,6 @@
 <?PHP
-$dbh = new PDO('mysql:host=45.76.106.37;dbname=jyutdict', 'jyut', '615v9qjVs1k8siMp');
+
+$dbh = new PDO('mysql:host=localhost;dbname=jyutdict', 'jyut', '615v9qjVs1k8siMp');
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $dbh->exec('SET NAMES UTF8MB4');
 ?>
