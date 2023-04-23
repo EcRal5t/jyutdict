48dac5378c088ccf03885fd40edb4f67f18e54a6
hethacoskras <526438991@qq.com>
2023-04-24 05:31:02 +0900

    更新字表信息欄和關於頁

--- const.php
+++ const.php
@@ -17,7 +17,7 @@ final class Info {
         $version = self::$version;
         echo <<< SIDENAV
     <style>.sidenav-info:after{content:
-    '主版本:{$version} \A 廣州190811 | 沙塘 210820 \A 北海190219 | 亭子190222 \A 貴港190127 | 欽州190819 \A 廉州190219 | 江門 201017 \A 梧州190117 | 蒼梧190118 \A 鬱林210915 | 茂名200709 \A 肇庆 201029 | 防城 190910 \A 尋旺 200126 | 容縣 2102 \A 廣韻 140930';}
+    '主版本:{$version} \A 鶴山沙坪-220601 \A 江門水南-220623 \A 江門白沙-220623 \A 江門沙仔尾-220623 \A 台山斗山墟-221024 \A 新會會城-220623 \A 開平赤磡-221020 \A 開平沙塘-211011 \A 江門墟頂-220623 \A 江門白話-221017 \A 廣州-190811 \A 順德大良-211021 \A 肇慶-201029 \A 梧州-181228 \A 桂平-210515 \A 南寧-210302 \A 高州-230308 \A 高州石鼓-230308 \A 茂名-200709 \A 防城-190910 \A 欽州-190820 \A 北海-190219 \A 廉州-190114 \A 湛江坡头-221013 \A 吳川吳陽-221013 \A 化州上江-220404 \A 化州下江-220404 \A 貴港街裏-190720 \A 桂平尋旺-200126 \A 梧州扶典-230129 \A 蒼梧石橋-181228 \A 容縣容州2102? \A 北流大旺-220608 \A 鬱林大塘-忘了 \A 蒙山-220321 \A 廣韻 140930';}
     </style>
     
     <div class="sidenav-overlay" onclick="hideSidenav()"></div>
@@ -29,12 +29,11 @@ final class Info {
         </div>
         
         <ul id="sidenav-list">
-            <li class="sidenav-link"><a href="./">字</a></li>
-            <li class="sidenav-link"><a href="./pron">音</a></li>
-            <li class="sidenav-link" style="text-decoration:line-through;"><a>　韻　</a></li>
-            <li class="sidenav-link" style="text-decoration:line-through;"><a>　詞　</a></li>
-            <li class="sidenav-link"><a href="https://docs.google.com/spreadsheets/d/1jwpB2pKBM0OfONJXHCRIvAhEl4cTEobRdbw6z1PajCQ/edit?usp=sharing" target="_blank">泛粵表<span style="font-size: 0.5em;">　需梯及電腦</span></a></li>
+            <li class="sidenav-link"><a href="./">檢字</a></li>
+            <li class="sidenav-link"><a href="./pron">檢音</a></li>
+            <li class="sidenav-link"><a href="https://docs.google.com/spreadsheets/d/1jwpB2pKBM0OfONJXHCRIvAhEl4cTEobRdbw6z1PajCQ/edit?usp=sharing" target="_blank">泛粵表<span style="font-size: 0.5em;">(v1907. 於谷歌)</span></a></li>
             <li class="divider"></li>
+            <li class="sidenav-link"><a href="https://jyutjam.org/" target="_blank">關於</a></li>
             <li class="sidenav-link sidenav-info"><a href="./about">說明</a></li>
         </ul>
     </div>
--- index.php
+++ index.php
@@ -71,7 +71,7 @@ if (!empty($_REQUEST['character'])) {
                 </form>
 				<?PHP # 展示 app 下载链接的 banner
 				if (empty($_REQUEST['character'])) {
-					echo "<div style='text-align:center;margin:20px;padding:10px;' class='general-bg'>是否在檢索“粵用漢字”？來試試<a href='https://jyutjam.org/jyutdict-android/' target='_blank'>泛粵典安卓版</a>！</div>";
+					echo "<div style='text-align:center;margin:20px;padding:10px;' class='general-bg'>想檢索泛粵表嗎？來試試<a href='https://jyutjam.org/jyutdict-android/' target='_blank'>泛粵典安卓版</a>！</div>";
 				} else {
 					$jyutOnlyCharas = "漚齙鎅抨儮涫嗍喐企跙俾涿竉呃𤠑冚㪗墊劘𠲺捋鉸諗𧟘笡撴㩄扽竭痕歪炕掬𠺘凼搇喫𢴒扤燶焫眲𦢊𢬿𢱕嬲䆟姣𡴀鐺仲嘢妗擤唥瀨竇誃㧬偈斟徛靚耖湴癐應襟䐁佊腯扻樽𨣇窿熝咩冇攰砧孭㵒泵㨃脌㫱甂漝緡柒𢲲䊆鍘譇𥐹匱蕹欮𠱓𨂾𠼮撩欱噏焗囈攴篤摱軚啽擸𧨾搥哋戇姩𢆡扂佮唞攞卅屌躉渠箍蚊笠撳獳罯蓊赧窞嚿係撈刏攀煠潷樖爇轆剢嘥擰閂晾睏熰漦摣跛騮畀趤沊屐𨂽嘈黐𧽤謦冞啱㩧羹㞓呔𧿒鵮胐撓抌腩𣼽搲盪䑜骾錔屙懵劏㗇揾梘𠝹𡚦敨罌𤸻䅺煲䠩𧬈叻瘟𪒬喎埞髀欶𧰵挽乸𢳂咁滐啖搑𨈇壅睇搏冧𢩦擳掣擁踭䟗廿寐懟燜搦褸𠽒溦𪘞逳蝨𨳒傾㥋㩒𤗈熛抵𨳍嘸𢫏淥㢥扱脷㧺攨搽沌鑊甩摷𡰪踸墟掂嘔漉𧿔杘𥌮拎揸丼慳𧦠檻𢴩腍韞欬㔶拗氼閪菢佢晏蛤呷猋仔擝鏜乜掹惗澀囝𤊒凹椏滮㨆䂿攬挃疴肶哴坎氹嗲嘅炆瞓𧶄癲㧻㧾㫰㞘褪窟潺穮沕傑孖搵𧕴揞揀";
 					if (strpos($jyutOnlyCharas, $_REQUEST['character']) !== false) {
